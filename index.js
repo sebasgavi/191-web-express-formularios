7e4d9fa592a6da300b425b7a98a02f52eadea869
Sebastián Gaviria <vagyriag@hotmail.com>
2019-04-12 07:30:57 -0500

    servidor inicial, código base

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.use(express.static('public'));
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+
+
+app.listen(3000);
