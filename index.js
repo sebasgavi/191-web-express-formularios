55a93abb318a7dd09349979d50dcf7134cc213fb
Sebastián Gaviria <vagyriag@hotmail.com>
2019-04-12 08:34:09 -0500

    formulario enviando información

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var exphbs = require('express-handlebars');
 var app = express();
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -16,7 +17,7 @@ app.get('/', function(request, response){
 });
 
 app.post('/login', function(request, response){
-    console.log('hola');
+    console.log(request.body);
     response.send('hola');
 });
 
